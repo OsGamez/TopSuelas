e16e0197333ec7e7e2af64d95374787b4e15197c
Oswaldo <oswaldo.silvestre10@gmail.com>
2019-06-26 09:58:08 -0500

    Kardex

---
+++ src/ObjectLayer/KardexCmp.java
@@ -0,0 +1,221 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ObjectLayer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author GATEWAY1-
+ */
+public class KardexCmp implements java.io.Serializable{
+    private String fechamov, fechadoc, serie,tipo, nombrecuenta,nombreproveedor,nombrealmacen,
+            docref,material,nombrematerial,nombreusuario,status,operacion,Ordenc;
+
+    public String getOrdenc() {
+        return Ordenc;
+    }
+
+    public void setOrdenc(String Ordenc) {
+        this.Ordenc = Ordenc;
+    }
+    private ArrayList<Movs_materiales> mat;
+
+    public ArrayList<Movs_materiales> getMat() {
+        return mat;
+    }
+
+    public void setMat(ArrayList<Movs_materiales> mat) {
+        this.mat = mat;
+    }
+
+    
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+    private int cuenta,subcuenta,folio,proveedor,almacen,cantidad,renglon,usuario;
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+    private float costo,importe;
+
+    public String getFechamov() {
+        return fechamov;
+    }
+
+    public void setFechamov(String fechamov) {
+        this.fechamov = fechamov;
+    }
+
+    public String getFechadoc() {
+        return fechadoc;
+    }
+
+    public void setFechadoc(String fechadoc) {
+        this.fechadoc = fechadoc;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombrecuenta() {
+        return nombrecuenta;
+    }
+
+    public void setNombrecuenta(String nombrecuenta) {
+        this.nombrecuenta = nombrecuenta;
+    }
+
+    public String getNombreproveedor() {
+        return nombreproveedor;
+    }
+
+    public void setNombreproveedor(String nombreproveedor) {
+        this.nombreproveedor = nombreproveedor;
+    }
+
+    public String getNombrealmacen() {
+        return nombrealmacen;
+    }
+
+    public void setNombrealmacen(String nombrealmacen) {
+        this.nombrealmacen = nombrealmacen;
+    }
+
+    public String getDocref() {
+        return docref;
+    }
+
+    public void setDocref(String docref) {
+        this.docref = docref;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getNombrematerial() {
+        return nombrematerial;
+    }
+
+    public void setNombrematerial(String nombrematerial) {
+        this.nombrematerial = nombrematerial;
+    }
+
+    public String getNombreusuario() {
+        return nombreusuario;
+    }
+
+    public void setNombreusuario(String nombreusuario) {
+        this.nombreusuario = nombreusuario;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(int cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public int getSubcuenta() {
+        return subcuenta;
+    }
+
+    public void setSubcuenta(int subcuenta) {
+        this.subcuenta = subcuenta;
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public int getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(int proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public int getAlmacen() {
+        return almacen;
+    }
+
+    public void setAlmacen(int almacen) {
+        this.almacen = almacen;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getRenglon() {
+        return renglon;
+    }
+
+    public void setRenglon(int renglon) {
+        this.renglon = renglon;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public float getImporte() {
+        return importe;
+    }
+
+    public void setImporte(float importe) {
+        this.importe = importe;
+    }
+}
---
+++ src/ObjectLayer/ObjectKardexCmp.java
@@ -0,0 +1,162 @@
+package ObjectLayer;
+
+import DataAccesLayer.Conexion;
+import DataAccesLayer.DB;
+import DataAccesLayer.Server;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ObjectKardexCmp {
+
+    PreparedStatement st = null;
+    ResultSet rs = null;
+//    Connection c = Server.getCmpPhylon();
+    DB db = new DB();
+    Connection c = db.Cmp();
+
+    public boolean KardexCmpAdd(int am, String Desc) {
+        try {
+            c.setAutoCommit(false);
+            st = c.prepareStatement("INSERT INTO KardexCmpes(KardexCmp,Descripcion)"
+                    + "VALUES(?,?)");
+            st.setInt(1, am);
+            st.setString(2, Desc);
+            st.executeUpdate();
+            c.commit();
+            st.close();
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public ArrayList<KardexCmp> KardexCmpGatAll() {
+        ArrayList<KardexCmp> listaKardexCmp = new ArrayList<>();
+        try {
+            st = c.prepareStatement("select top(30) folio,k.cuenta,k.subcuenta,fechamov,fechadoc,serie,k.tipo,"
+                    + "k.ProveedorMPrima,k.Almacen,docref,k.cvemat,k.cantidad,k.costo,importe,\n"
+                    + "k.usuario,k.status,renglon,Ordencompra,mat.descripcion as 'mdescripcion',pmp.nombre as 'ndescripcion'"
+                    + ",alm.descripcion as 'adescripcion'\n"
+                    + "from KardexCmp k join  ConceptosES c on k.cuenta=c.Cuenta\n"
+                    + "join ProvedoresMPrima pmp on k.ProveedorMPrima=pmp.Proveedor\n"
+                    + "join Almacenes alm on k.Almacen=alm.Almacen\n"
+                    + "join Materiales mat on k.Cvemat=mat.CveMat  ORDER BY folio DESC");
+            rs = st.executeQuery();
+            while (rs.next()) {
+                KardexCmp k = new KardexCmp();
+                k.setFolio(rs.getInt("Folio"));
+                k.setCuenta(rs.getInt("cuenta"));
+                k.setFechamov(rs.getString("fechamov"));
+                k.setSerie(rs.getString("serie"));
+                k.setNombrealmacen(rs.getString("adescripcion"));
+                k.setNombrematerial(rs.getString("mdescripcion"));
+                k.setCantidad(rs.getInt("cantidad"));
+                listaKardexCmp.add(k);
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return listaKardexCmp;
+    }
+
+    public ArrayList<KardexCmp> KardexCmpSearch(String filtro) {
+        ArrayList<KardexCmp> listaKardexCmp = new ArrayList<>();
+        try {
+            st = c.prepareStatement("select top(30) folio,k.cuenta,k.subcuenta,fechamov,fechadoc,serie,k.tipo,"
+                    + "k.ProveedorMPrima,k.Almacen,docref,k.cvemat,k.cantidad,k.costo,importe,\n"
+                    + "k.usuario,k.status,renglon,Ordencompra,mat.descripcion as 'mdescripcion',pmp.nombre as 'ndescripcion'"
+                    + ",alm.descripcion as 'adescripcion'\n"
+                    + "from KardexCmp k join  ConceptosES c on k.cuenta=c.Cuenta\n"
+                    + "join ProvedoresMPrima pmp on k.ProveedorMPrima=pmp.Proveedor\n"
+                    + "join Almacenes alm on k.Almacen=alm.Almacen\n"
+                    + "join Materiales mat on k.Cvemat=mat.CveMat"
+                    + " where folio like '%" + filtro + "%' or mat.descripcion like '%" + filtro + "%' "
+                    + "or pmp.nombre like '%" + filtro + "%'  ORDER BY folio DESC");
+            rs = st.executeQuery();
+            while (rs.next()) {
+                KardexCmp k = new KardexCmp();
+                k.setFolio(rs.getInt("Folio"));
+                k.setCuenta(rs.getInt("cuenta"));
+                k.setFechamov(rs.getString("fechamov"));
+                k.setSerie(rs.getString("serie"));
+                k.setNombrealmacen(rs.getString("adescripcion"));
+                k.setNombrematerial(rs.getString("mdescripcion"));
+                k.setCantidad(rs.getInt("cantidad"));
+                listaKardexCmp.add(k);
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return listaKardexCmp;
+    }
+
+    public boolean KardexCmpUpdate(String Desc, int am) {
+        try {
+            c.setAutoCommit(false);
+            st = c.prepareStatement("UPDATE KardexCmpes SET Descripcion =? WHERE KardexCmp = ?");
+            st.setString(1, Desc);
+            st.setInt(2, am);
+            st.executeUpdate();
+            c.commit();
+            st.close();
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean KardexCmpDelete(int Id, String Des) {
+        try {
+            c.setAutoCommit(false);
+            st = c.prepareStatement("DELETE FROM KardexCmpes WHERE KardexCmp = ? AND Descripcion = ?");
+            st.setInt(1, Id);
+            st.setString(2, Des);
+            st.execute();
+            c.commit();
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public ArrayList<KardexCmp> getById(String id) {
+        ArrayList<KardexCmp> listaKardexCmp = new ArrayList<KardexCmp>();
+        try {
+            st = c.prepareStatement("SELECT KardexCmp  FROM KardexCmpes WHERE KardexCmp =?");
+            st.setString(1, id);
+            rs = st.executeQuery();
+
+            while (rs.next()) {
+                KardexCmp am = new KardexCmp();
+                listaKardexCmp.add(am);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return listaKardexCmp;
+    }
+     public String getparametro(String columna) {
+        String parametro ="";
+        try {
+            st = c.prepareStatement("SELECT "+columna+" FROM parametroscmp");
+            rs = st.executeQuery();
+
+            while (rs.next()) {
+                parametro=rs.getString(columna);
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return parametro;
+    }
+}
